cf425f94bf31d92f7cb4635406e7e3c42a591ded
Shovnik Bhattacharya <32278173+shovnik@users.noreply.github.com>
2017-10-02 18:22:40 -0400

    Update CommandLineArguments.cpp

--- 1. Introduction/CommandLineArguments.cpp
+++ 1. Introduction/CommandLineArguments.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 int main(const int argc, const char* const argv[])
 {
-	cout << "In the array argc, inputted command line arguments are stored." << endl;
-	cout << "The value stored in argc is " << argc << "." << endl;
-	cout << "The variable \"argc\" contains the number of command line arguments." << endl;
+    cout << "In the array argc, inputted command line arguments are stored." << endl;
+    cout << "The value stored in argc is " << argc << "." << endl;
+    cout << "The variable \"argc\" contains the number of command line arguments." << endl;
 
-	return 0;
+    return 0;
 }
+
