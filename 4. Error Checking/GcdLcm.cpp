c45a07c606e2a75f74d1aa6d53f5bbd3967414db
Shovnik Bhattacharya <32278173+shovnik@users.noreply.github.com>
2017-10-02 18:26:16 -0400

    Update GcdLcm.cpp

--- 4. Error Checking/GcdLcm.cpp
+++ 4. Error Checking/GcdLcm.cpp
@@ -5,42 +5,42 @@ using namespace std;
 
 int main(const int argc, const char* const argv[])
 {
-	if (argc < 3)
-	{
-		cerr << "Error: Unable to compute GCD and/or LCM because there is an insufficient amount "
-			"of arguments."
-			<< endl;
-	}
-	else
-	{
-		if (argc > 3)
-		{
-			cerr << "Warning: Expecting two command-line arguments; received " << (argc - 1)
-				<< "; ignoring extraneous arguments." << endl;
-		}
-		int a = atoi(argv[1]);
-		int b = atoi(argv[2]);
-		if (a < 1 || b < 1)
-		{
-			cerr << "Error: Unable to compute GCD and/or LCM because a negative number or zera was "
-				"used as input."
-				<< endl;
-		}
-		else
-		{
-			int gcd = a;
-			int s = b;
-			int t;
-			while (s != 0)
-			{
-				t = s;
-				s = gcd % s;
-				gcd = t;
-			}
-			int lcm = (a / gcd) * b;
+    if (argc < 3)
+    {
+        cerr << "Error: Unable to compute GCD and/or LCM because there is an insufficient amount "
+                "of arguments."
+             << endl;
+    }
+    else
+    {
+        if (argc > 3)
+        {
+            cerr << "Warning: Expecting two command-line arguments; received " << (argc - 1)
+                 << "; ignoring extraneous arguments." << endl;
+        }
+        int a = atoi(argv[1]);
+        int b = atoi(argv[2]);
+        if (a < 1 || b < 1)
+        {
+            cerr << "Error: Unable to compute GCD and/or LCM because a negative number or zera was "
+                    "used as input."
+                 << endl;
+        }
+        else
+        {
+            int gcd = a;
+            int s = b;
+            int t;
+            while (s != 0)
+            {
+                t = s;
+                s = gcd % s;
+                gcd = t;
+            }
+            int lcm = (a / gcd) * b;
 
-			cout << "GCD (" << a << "," << b << ") = " << gcd << endl;
-			cout << "LCM (" << a << "," << b << ") = " << lcm << endl;
-		}
-	}
+            cout << "GCD (" << a << "," << b << ") = " << gcd << endl;
+            cout << "LCM (" << a << "," << b << ") = " << lcm << endl;
+        }
+    }
 }
