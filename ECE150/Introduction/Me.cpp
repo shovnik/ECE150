f3268bbeda59f5eb0df51c97084142dcab744b10
Shovnik Bhattacharya <32278173+shovnik@users.noreply.github.com>
2017-10-02 17:01:40 -0400

    Update and rename ECE150/Lab 0/Me.cpp to ECE150/Introduction/Me.cpp

---
+++ ECE150/Introduction/Me.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+
+
+	cout << "My name is Shovnik Bhattacharya." << endl;
+	cout << "I am in Computer Engineering." << endl;
+	cout << "I like programming because it is logical and methodical, yet it allows creativity.." << endl;
+
+	return 0;
+}
