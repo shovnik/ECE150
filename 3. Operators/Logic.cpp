35e2ebadf7f52dce902cc60ce5e999bd6e91b17b
Shovnik Bhattacharya <32278173+shovnik@users.noreply.github.com>
2017-10-02 18:25:13 -0400

    Update Logic.cpp

--- 3. Operators/Logic.cpp
+++ 3. Operators/Logic.cpp
@@ -8,18 +8,18 @@ using namespace std;
 
 int main(const int argc, const char* const argv[])
 {
-	int P;
-	int Q;
+    int P;
+    int Q;
 
-	P = atoi(argv[1]);
-	Q = atoi(argv[2]);
+    P = atoi(argv[1]);
+    Q = atoi(argv[2]);
 
-	cout << "P: " << P << endl;
-	cout << "Q: " << Q << endl;
-	cout << "P AND Q: " << (P && Q) << endl;
-	cout << "P OR Q: " << (P || Q) << endl;
-	cout << "NOT P: " << (!P) << endl;
-	cout << "NOT Q: " << (!Q) << endl;
+    cout << "P: " << P << endl;
+    cout << "Q: " << Q << endl;
+    cout << "P AND Q: " << (P && Q) << endl;
+    cout << "P OR Q: " << (P || Q) << endl;
+    cout << "NOT P: " << (!P) << endl;
+    cout << "NOT Q: " << (!Q) << endl;
 
-	return 0;
+    return 0;
 }
