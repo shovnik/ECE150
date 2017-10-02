f42ba9c5ee483429e55c570d2bc3bccee8b619f5
Shovnik Bhattacharya <32278173+shovnik@users.noreply.github.com>
2017-10-02 17:08:59 -0400

    Update and rename Welcome.cpp to CommandLineArguments.cpp

--- ECE150/Introduction/CommandLineArguments.cpp
+++ ECE150/Introduction/CommandLineArguments.cpp
@@ -6,12 +6,7 @@ using namespace std;
 
 int main(const int argc, const char* const argv[])
 {
-
-
-	cout << "Welcome to UW Engineering, " << argv[1] << "." << endl;
-	cout << "Your University of Waterloo E-mail address is: " << argv[2] << "@uwaterloo.ca" << endl;
-
-
+	cout << "In the array argc, inputted command line arguments are stored." << endl;
 	cout << "The value stored in argc is " << argc << "." << endl;
 	cout << "The variable \"argc\" contains the number of command line arguments." << endl;
 
