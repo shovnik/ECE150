42e916e95a6d6dda5b7e2de5b4072736506f4d85
Shovnik Bhattacharya <32278173+shovnik@users.noreply.github.com>
2017-10-02 18:26:53 -0400

    Update Running.cpp

--- 6. Statistics/Running.cpp
+++ 6. Statistics/Running.cpp
@@ -6,55 +6,55 @@ using namespace std;
 
 int main(const int argc, const char* const argv[])
 {
-	if (argc < 2)
-	{
-		cerr << "Unable to compute statistics over data set because data set is empty." << endl;
-	}
+    if (argc < 2)
+    {
+        cerr << "Unable to compute statistics over data set because data set is empty." << endl;
+    }
 
-	float sum = 0; // sum of floating-point numbers
-	float avg = 0; // average of floating-point numbers
-	float min = atof(argv[1]); // minimum floating-point number
-	float max = atof(argv[1]); // maximum floating-point number
+    float sum = 0; // sum of floating-point numbers
+    float avg = 0; // average of floating-point numbers
+    float min = atof(argv[1]); // minimum floating-point number
+    float max = atof(argv[1]); // maximum floating-point number
 
-	const char s = ' '; // separator
-	const int w = 10; // width per slot
+    const char s = ' '; // separator
+    const int w = 10; // width per slot
 
-	cout << left << setw(w) << setfill(s) << "Sample";
-	cout << left << setw(w) << setfill(s) << "Value";
-	cout << left << setw(w) << setfill(s) << "Minimum";
-	cout << left << setw(w) << setfill(s) << "Average";
-	cout << left << setw(w) << setfill(s) << "Maximum";
-	cout << endl;
+    cout << left << setw(w) << setfill(s) << "Sample";
+    cout << left << setw(w) << setfill(s) << "Value";
+    cout << left << setw(w) << setfill(s) << "Minimum";
+    cout << left << setw(w) << setfill(s) << "Average";
+    cout << left << setw(w) << setfill(s) << "Maximum";
+    cout << endl;
 
 
-	int i = 1; // index for while loop
-	while (i <= argc - 1)
-	{
-		sum += atof(argv[i]);
-		avg = sum / i;
+    int i = 1; // index for while loop
+    while (i <= argc - 1)
+    {
+        sum += atof(argv[i]);
+        avg = sum / i;
 
-		if (atof(argv[i]) < min)
-		{
-			min = atof(argv[i]);
-		}
+        if (atof(argv[i]) < min)
+        {
+            min = atof(argv[i]);
+        }
 
-		if (atof(argv[i]) > max)
-		{
-			max = atof(argv[i]);
-		}
+        if (atof(argv[i]) > max)
+        {
+            max = atof(argv[i]);
+        }
 
-		cout << left << setw(w) << setfill(s) << i;
-		cout << left << setw(w) << setfill(s) << atof(argv[i]);
-		cout << left << setw(w) << setfill(s) << min;
-		cout << left << setw(w) << setfill(s) << avg;
-		cout << left << setw(w) << setfill(s) << max;
-		cout << endl;
+        cout << left << setw(w) << setfill(s) << i;
+        cout << left << setw(w) << setfill(s) << atof(argv[i]);
+        cout << left << setw(w) << setfill(s) << min;
+        cout << left << setw(w) << setfill(s) << avg;
+        cout << left << setw(w) << setfill(s) << max;
+        cout << endl;
 
-		i += 1;
-	}
+        i += 1;
+    }
 
-	cout << "Number of floating-point numbers: " << num << endl;
-	cout << "Minimum floating-point number: " << min << endl;
-	cout << "Average floating-point number: " << avg << endl;
-	cout << "Maximum floating-point number: " << max << endl;
+    cout << "Number of floating-point numbers: " << num << endl;
+    cout << "Minimum floating-point number: " << min << endl;
+    cout << "Average floating-point number: " << avg << endl;
+    cout << "Maximum floating-point number: " << max << endl;
 }
